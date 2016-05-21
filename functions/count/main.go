f814401ff521d819d88be5ada70c64e8aa3dcc7d
Brad Gignac <bgignac@bradgignac.com>
2016-05-21 00:45:15 -0400

    Reset counter on long press.

--- functions/count/main.go
+++ functions/count/main.go
@@ -11,12 +11,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
-const table = "counters"
-
 type message struct {
-	SerialNumber   string `json:"serialNumber"`
-	BatteryVoltage string `json:"batteryVoltage"`
-	ClickType      string `json:"clickType"`
+	ClickType string `json:"clickType"`
 }
 
 func main() {
@@ -28,14 +24,21 @@ func main() {
 		}
 
 		id := os.Getenv("counterId")
-		region := os.Getenv("region")
-		client := createDynamoClient(region)
+		client := createDynamoClient()
 
-		return incrementCounter(client, id)
+		switch m.ClickType {
+		case "SINGLE":
+			return incrementCounter(client, id)
+		case "LONG":
+			return resetCounter(client, id)
+		}
+
+		return nil, nil
 	})
 }
 
-func createDynamoClient(region string) *dynamodb.DynamoDB {
+func createDynamoClient() *dynamodb.DynamoDB {
+	region := os.Getenv("region")
 	session := session.New()
 	config := aws.NewConfig().WithRegion(region)
 	dynamo := dynamodb.New(session, config)
@@ -43,22 +46,37 @@ func createDynamoClient(region string) *dynamodb.DynamoDB {
 }
 
 func incrementCounter(dynamo *dynamodb.DynamoDB, id string) (int64, error) {
+	return updateCounter(dynamo, id, map[string]*dynamodb.AttributeValueUpdate{
+		"count": {
+			Action: aws.String("ADD"),
+			Value: &dynamodb.AttributeValue{
+				N: aws.String("1"),
+			},
+		},
+	})
+}
+
+func resetCounter(dynamo *dynamodb.DynamoDB, id string) (int64, error) {
+	return updateCounter(dynamo, id, map[string]*dynamodb.AttributeValueUpdate{
+		"count": {
+			Action: aws.String("PUT"),
+			Value: &dynamodb.AttributeValue{
+				N: aws.String("0"),
+			},
+		},
+	})
+}
+
+func updateCounter(dynamo *dynamodb.DynamoDB, id string, update map[string]*dynamodb.AttributeValueUpdate) (int64, error) {
 	params := &dynamodb.UpdateItemInput{
-		TableName: aws.String(table),
+		TableName: aws.String("counters"),
 		Key: map[string]*dynamodb.AttributeValue{
 			"counter_id": {
 				S: aws.String(id),
 			},
 		},
-		AttributeUpdates: map[string]*dynamodb.AttributeValueUpdate{
-			"count": {
-				Action: aws.String("ADD"),
-				Value: &dynamodb.AttributeValue{
-					N: aws.String("1"),
-				},
-			},
-		},
-		ReturnValues: aws.String("UPDATED_NEW"),
+		AttributeUpdates: update,
+		ReturnValues:     aws.String("UPDATED_NEW"),
 	}
 
 	resp, err := dynamo.UpdateItem(params)
@@ -68,7 +86,5 @@ func incrementCounter(dynamo *dynamodb.DynamoDB, id string) (int64, error) {
 
 	countAttr := resp.Attributes["count"]
 	countValue := aws.StringValue(countAttr.N)
-	countInt, err := strconv.ParseInt(countValue, 10, 64)
-
-	return countInt, err
+	return strconv.ParseInt(countValue, 10, 64)
 }
