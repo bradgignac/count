6b5cdccbf071636ad5418cabb74478235390c589
Brad Gignac <bgignac@bradgignac.com>
2016-05-24 01:04:40 -0400

    Update device shadow when count changes.

--- functions/count/main.go
+++ functions/count/main.go
@@ -9,10 +9,24 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/iot"
+	"github.com/aws/aws-sdk-go/service/iotdataplane"
 )
 
 type message struct {
 	ClickType string `json:"clickType"`
+}
+
+var region = os.Getenv("region")
+var endpoint = os.Getenv("endpoint")
+var iotCtrl *iot.IoT
+var iotData *iotdataplane.IoTDataPlane
+var dynamo *dynamodb.DynamoDB
+
+func init() {
+	iotCtrl = createIotClient()
+	iotData = createIotDataplaneClient()
+	dynamo = createDynamoClient()
 }
 
 func main() {
@@ -24,29 +38,52 @@ func main() {
 		}
 
 		id := os.Getenv("counterId")
-		client := createDynamoClient()
 
 		switch m.ClickType {
-		case "SINGLE":
-			return incrementCounter(client, id)
 		case "LONG":
-			return resetCounter(client, id)
+			return createCounter(id)
+		case "SINGLE":
+			return incrementCounter(id)
+		case "DOUBLE":
+			return resetCounter(id)
 		}
 
 		return nil, nil
 	})
 }
 
-func createDynamoClient() *dynamodb.DynamoDB {
-	region := os.Getenv("region")
+func createIotClient() *iot.IoT {
 	session := session.New()
 	config := aws.NewConfig().WithRegion(region)
-	dynamo := dynamodb.New(session, config)
-	return dynamo
+	return iot.New(session, config)
 }
 
-func incrementCounter(dynamo *dynamodb.DynamoDB, id string) (int64, error) {
-	return updateCounter(dynamo, id, map[string]*dynamodb.AttributeValueUpdate{
+func createIotDataplaneClient() *iotdataplane.IoTDataPlane {
+	session := session.New()
+	config := aws.NewConfig().
+		WithRegion(region).
+		WithEndpoint(endpoint)
+	return iotdataplane.New(session, config)
+}
+
+func createDynamoClient() *dynamodb.DynamoDB {
+	session := session.New()
+	config := aws.NewConfig().WithRegion(region)
+	return dynamodb.New(session, config)
+}
+
+func createCounter(id string) (string, error) {
+	params := &iot.CreateThingInput{ThingName: aws.String(id)}
+	resp, err := iotCtrl.CreateThing(params)
+	if err != nil {
+		return "", err
+	}
+
+	return aws.StringValue(resp.ThingArn), nil
+}
+
+func incrementCounter(id string) (int64, error) {
+	count, err := updateCounter(id, map[string]*dynamodb.AttributeValueUpdate{
 		"count": {
 			Action: aws.String("ADD"),
 			Value: &dynamodb.AttributeValue{
@@ -54,10 +91,15 @@ func incrementCounter(dynamo *dynamodb.DynamoDB, id string) (int64, error) {
 			},
 		},
 	})
+	if err != nil {
+		return 0, err
+	}
+
+	return updateShadow(id, count)
 }
 
-func resetCounter(dynamo *dynamodb.DynamoDB, id string) (int64, error) {
-	return updateCounter(dynamo, id, map[string]*dynamodb.AttributeValueUpdate{
+func resetCounter(id string) (int64, error) {
+	count, err := updateCounter(id, map[string]*dynamodb.AttributeValueUpdate{
 		"count": {
 			Action: aws.String("PUT"),
 			Value: &dynamodb.AttributeValue{
@@ -65,9 +107,14 @@ func resetCounter(dynamo *dynamodb.DynamoDB, id string) (int64, error) {
 			},
 		},
 	})
+	if err != nil {
+		return 0, err
+	}
+
+	return updateShadow(id, count)
 }
 
-func updateCounter(dynamo *dynamodb.DynamoDB, id string, update map[string]*dynamodb.AttributeValueUpdate) (int64, error) {
+func updateCounter(id string, update map[string]*dynamodb.AttributeValueUpdate) (int64, error) {
 	params := &dynamodb.UpdateItemInput{
 		TableName: aws.String("counters"),
 		Key: map[string]*dynamodb.AttributeValue{
@@ -78,7 +125,6 @@ func updateCounter(dynamo *dynamodb.DynamoDB, id string, update map[string]*dyna
 		AttributeUpdates: update,
 		ReturnValues:     aws.String("UPDATED_NEW"),
 	}
-
 	resp, err := dynamo.UpdateItem(params)
 	if err != nil {
 		return 0, err
@@ -87,4 +133,26 @@ func updateCounter(dynamo *dynamodb.DynamoDB, id string, update map[string]*dyna
 	countAttr := resp.Attributes["count"]
 	countValue := aws.StringValue(countAttr.N)
 	return strconv.ParseInt(countValue, 10, 64)
+}
+
+func updateShadow(id string, count int64) (int64, error) {
+	shadow := &map[string]interface{}{
+		"state": &map[string]interface{}{
+			"desired": &map[string]interface{}{"count": count},
+		},
+	}
+	payload, err := json.Marshal(shadow)
+	if err != nil {
+		return 0, err
+	}
+
+	params := &iotdataplane.UpdateThingShadowInput{
+		ThingName: aws.String(id),
+		Payload:   payload,
+	}
+	if _, err = iotData.UpdateThingShadow(params); err != nil {
+		return 0, err
+	}
+
+	return count, nil
 }
