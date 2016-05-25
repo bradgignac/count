591f93536b85abb8ace7a9c3710294277bdeeee9
Brad Gignac <bgignac@bradgignac.com>
2016-05-24 22:36:46 -0400

    Wire up client to IoT updates.

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const iot = require('aws-iot-device-sdk');
 
 const region = 'us-east-1';
 const pool = 'us-east-1:594b1376-9d0e-4adb-9f91-1a3fae53d247';
+const counterId = 'counter';
+
+let shadow;
 
 aws.config.region = region;
 aws.config.credentials = new aws.CognitoIdentityCredentials({ IdentityPoolId: pool });
@@ -11,15 +14,32 @@ aws.config.credentials.get((err, data) => {
     return;
   }
 
-  const credentials = aws.config.credentials;
-  const shadow = iot.thingShadow({
-   region: region,
-   clientId: 'count-client-' + (Math.floor((Math.random() * 100000) + 1)),
-   protocol: 'wss',
-   maximumReconnectTimeMs: 8000,
-   debug: true,
-   accessKeyId: credentials.accessKeyId,
-   secretKey: credentials.secretAccessKey,
-   sessionToken: credentials.sessionToken
+  shadow = iot.thingShadow({
+    region: region,
+    clientId: 'count-client-' + (Math.floor((Math.random() * 100000) + 1)),
+    protocol: 'wss',
+    accessKeyId: aws.config.credentials.accessKeyId,
+    secretKey: aws.config.credentials.secretAccessKey,
+    sessionToken: aws.config.credentials.sessionToken
   });
+
+  shadow.on('connect', onShadowConnected);
+  shadow.on('status', onShadowStatus);
+  shadow.on('delta', onShadowDelta);
 });
+
+const onShadowConnected = () => {
+  shadow.register(counterId, { persistentSubscribe: true });
+  setTimeout(() => shadow.get(counterId), 1000);
+};
+
+const onShadowStatus = (name, status, token, data) => {
+  console.log('Received IoT status update!');
+  document.body.classList.remove('loading');
+  document.getElementById('count').textContent = data.state.desired.count;
+};
+
+const onShadowDelta = (name, data) => {
+  console.log('Received IoT state delta!');
+  document.getElementById('count').textContent = data.state.count;
+};
